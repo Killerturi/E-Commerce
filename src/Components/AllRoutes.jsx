a38b1b3705660b2c0add11eecaae1f7dae0e23b3
Killerturi <110190885+Killerturi@users.noreply.github.com>
2023-02-20 00:12:44 +0530

    new update

--- src/Components/AllRoutes.jsx
+++ src/Components/AllRoutes.jsx
@@ -1,21 +1,17 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
-import NavBar from "./NavBar";
 import Login from "./Sign_up_in/Login";
 import SignUp from "./Sign_up_in/SignUp";
 
 const AllRoutes = () => {
   return (
     <>
-      <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
     </>
   );
 };
