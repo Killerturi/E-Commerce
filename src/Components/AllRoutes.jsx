e7be1e0fd60e4196a854235abdb414910d6a9718
Malaywebdeveloper <125017719+Malaywebdeveloper@users.noreply.github.com>
2023-02-28 22:30:11 +0530

    Route add

--- src/Components/AllRoutes.jsx
+++ src/Components/AllRoutes.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Sign_up_in/Login";
 import SignUp from "./Sign_up_in/SignUp";
 import Home from "../Home/Home"
+import PrivateRoute from "./privateRoute/PrivateRoute";
+import Products from "../Products/Products";
+import SingleProduct from "../SingleProduct/SingleProduct";
 
 const AllRoutes = () => {
   return (
@@ -12,6 +15,32 @@ const AllRoutes = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route
+          path="/mobilesandtablets"
+          element={<PrivateRoute><Products typeOfProduct="mobilesandtablets" /></PrivateRoute>}
+        ></Route>
+        <Route
+          path="/mobilesandtablets/:id"
+          element={<PrivateRoute><SingleProduct typeOfProduct="mobilesandtablets" /></PrivateRoute>}
+        ></Route>
+
+        <Route
+          path="/televisions"
+         element={<PrivateRoute><Products typeOfProduct="television"/></PrivateRoute>}
+        ></Route>
+        <Route
+          path="/televisions/:id"
+         element={<PrivateRoute><SingleProduct typeOfProduct="television"/></PrivateRoute>}
+        ></Route>
+
+        <Route
+        path="/kitchen"
+        element={<PrivateRoute><Products typeOfProduct="kitchen"/></PrivateRoute>}
+        ></Route>
+        <Route
+        path="/kitchen/:id"
+        element={<PrivateRoute><SingleProduct typeOfProduct="kitchen"/></PrivateRoute>}
+        ></Route>
       </Routes>
     </>
   );
