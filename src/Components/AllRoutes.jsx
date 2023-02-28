c21ce2dc8b8955fe987ec5a0ebd2029911e99094
Malaywebdeveloper <125017719+Malaywebdeveloper@users.noreply.github.com>
2023-02-28 22:47:47 +0530

    Hi

--- src/Components/AllRoutes.jsx
+++ src/Components/AllRoutes.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./Sign_up_in/Login";
 import SignUp from "./Sign_up_in/SignUp";
-import Home from "../Home/Home"
+import Home from "../Home/Home";
 import PrivateRoute from "./privateRoute/PrivateRoute";
 import Products from "../Products/Products";
 import SingleProduct from "../SingleProduct/SingleProduct";
@@ -17,29 +17,149 @@ const AllRoutes = () => {
         <Route path="/signup" element={<SignUp />} />
         <Route
           path="/mobilesandtablets"
-          element={<PrivateRoute><Products typeOfProduct="mobilesandtablets" /></PrivateRoute>}
+          element={
+            <PrivateRoute>
+              <Products typeOfProduct="mobilesandtablets" />
+            </PrivateRoute>
+          }
         ></Route>
         <Route
           path="/mobilesandtablets/:id"
-          element={<PrivateRoute><SingleProduct typeOfProduct="mobilesandtablets" /></PrivateRoute>}
+          element={
+            <PrivateRoute>
+              <SingleProduct typeOfProduct="mobilesandtablets" />
+            </PrivateRoute>
+          }
         ></Route>
 
         <Route
           path="/televisions"
-         element={<PrivateRoute><Products typeOfProduct="television"/></PrivateRoute>}
+          element={
+            <PrivateRoute>
+              <Products typeOfProduct="television" />
+            </PrivateRoute>
+          }
         ></Route>
         <Route
           path="/televisions/:id"
-         element={<PrivateRoute><SingleProduct typeOfProduct="television"/></PrivateRoute>}
+          element={
+            <PrivateRoute>
+              <SingleProduct typeOfProduct="television" />
+            </PrivateRoute>
+          }
         ></Route>
 
         <Route
-        path="/kitchen"
-        element={<PrivateRoute><Products typeOfProduct="kitchen"/></PrivateRoute>}
+          path="/kitchen"
+          element={
+            <PrivateRoute>
+              <Products typeOfProduct="kitchen" />
+            </PrivateRoute>
+          }
         ></Route>
         <Route
-        path="/kitchen/:id"
-        element={<PrivateRoute><SingleProduct typeOfProduct="kitchen"/></PrivateRoute>}
+          path="/kitchen/:id"
+          element={
+            <PrivateRoute>
+              <SingleProduct typeOfProduct="kitchen" />
+            </PrivateRoute>
+          }
+        ></Route>
+        <Route
+          path="/headphones"
+          element={
+            <PrivateRoute>
+              <Products typeOfProduct="headphones" />
+            </PrivateRoute>
+          }
+        ></Route>
+        <Route
+          path="/headphones/:id"
+          element={
+            <PrivateRoute>
+              <SingleProduct typeOfProduct="headphones" />
+            </PrivateRoute>
+          }
+        ></Route>
+        <Route
+          path="/homeappliances"
+          element={
+            <PrivateRoute>
+              <Products typeOfProduct="homeappliances" />
+            </PrivateRoute>
+          }
+        ></Route>
+        <Route
+          path="/homeappliances/:id"
+          element={
+            <PrivateRoute>
+              <SingleProduct typeOfProduct="homeappliances" />
+            </PrivateRoute>
+          }
+        ></Route>
+        <Route
+          path="/computers"
+          element={
+            <PrivateRoute>
+              <Products typeOfProduct="computers" />
+            </PrivateRoute>
+          }
+        ></Route>
+        <Route
+          path="/computers/:id"
+          element={
+            <PrivateRoute>
+              <SingleProduct typeOfProduct="computers" />
+            </PrivateRoute>
+          }
+        ></Route>
+        <Route
+          path="/cameras"
+          element={
+            <PrivateRoute>
+              <Products typeOfProduct="cameras" />
+            </PrivateRoute>
+          }
+        ></Route>
+        <Route
+          path="/cameras/:id"
+          element={
+            <PrivateRoute>
+              <SingleProduct typeOfProduct="cameras" />
+            </PrivateRoute>
+          }
+        ></Route>
+        <Route
+          path="/personalcare"
+          element={
+            <PrivateRoute>
+              <Products typeOfProduct="personalcare" />
+            </PrivateRoute>
+          }
+        ></Route>
+        <Route
+          path="/personalcare/:id"
+          element={
+            <PrivateRoute>
+              <SingleProduct typeOfProduct="personalcare" />
+            </PrivateRoute>
+          }
+        ></Route>
+        <Route
+          path="/accessories"
+          element={
+            <PrivateRoute>
+              <Products typeOfProduct="accessories" />
+            </PrivateRoute>
+          }
+        ></Route>
+        <Route
+          path="/accessories/:id"
+          element={
+            <PrivateRoute>
+              <SingleProduct typeOfProduct="accessories" />
+            </PrivateRoute>
+          }
         ></Route>
       </Routes>
     </>
